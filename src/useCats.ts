0f8f55f1c0c8347faad6771a92da80f0c706d630
pontakornth <pontakorn61@gmail.com>
2021-03-31 19:25:13 +0700

    fix: Prevent refetching

--- src/useCats.ts
+++ src/useCats.ts
@@ -1,13 +1,13 @@
 import useSWRV from 'swrv'
 import LocalStorageCache from 'swrv/dist/cache/adapters/localStorage'
+const fetcher = (url: string, options: Parameters<typeof fetch>[1]) => fetch(url, options).then(res => res.json())
 const useCats = () => {
     return useSWRV(['https://api.thecatapi.com/v1/images/search?limit=20&order=ASC', {
         headers: {
             'x-api-key': import.meta.env.CAT_API_KEY as string
         }
-    }], undefined, {
+    }], fetcher, {
         revalidateOnFocus: false,
-        dedupingInterval: 30000,
         shouldRetryOnError: false,
         cache: new LocalStorageCache(),
     })
