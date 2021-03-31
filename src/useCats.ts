0680e72a3220cd1273bb74e81e067d14867d4c4e
pontakornth <pontakorn61@gmail.com>
2021-03-31 16:50:38 +0700

    feat: Add cache

--- src/useCats.ts
+++ src/useCats.ts
@@ -1,11 +1,15 @@
 import useSWRV from 'swrv'
+import LocalStorageCache from 'swrv/dist/cache/adapters/localStorage'
 const useCats = () => {
-    return useSWRV(['https://api.thecatapi.com/v1/images/search?limit=20?order=ASC', {
+    return useSWRV(['https://api.thecatapi.com/v1/images/search?limit=20&order=ASC', {
         headers: {
             'x-api-key': import.meta.env.CAT_API_KEY as string
         }
-    }], fetch, {
-        revalidateOnFocus: true
+    }], undefined, {
+        revalidateOnFocus: false,
+        dedupingInterval: 30000,
+        shouldRetryOnError: false,
+        cache: new LocalStorageCache(),
     })
 }
 
