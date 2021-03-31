7acc94318aff333a7a1a921f619f2bd0da053e11
pontakornth <pontakorn61@gmail.com>
2021-03-31 16:29:13 +0700

    fix: Too many request

--- src/useCats.ts
+++ src/useCats.ts
@@ -1,10 +1,12 @@
 import useSWRV from 'swrv'
 const useCats = () => {
-    return useSWRV(['https://api.thecatapi.com/v1/images/search?limit=20', {
+    return useSWRV(['https://api.thecatapi.com/v1/images/search?limit=20?order=ASC', {
         headers: {
             'x-api-key': import.meta.env.CAT_API_KEY as string
         }
-    }])
+    }], fetch, {
+        revalidateOnFocus: true
+    })
 }
 
 export default useCats
