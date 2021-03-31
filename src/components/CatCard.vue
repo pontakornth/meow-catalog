8f00c795483a324bcae6f5dcb0ca3249c9f0e9a1
pontakornth <pontakorn61@gmail.com>
2021-03-31 16:14:56 +0700

    feat: Add cat card

---
+++ src/components/CatCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="p-4 rounded border shadow-md">
+        <img :src="imageUrl" alt="Cat(s)" />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        imageUrl: {
+            type: String,
+            required: true,
+        },
+    },
+    setup() {
+        
+    },
+})
+</script>
