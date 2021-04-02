4ec94d7d0ba4a05b377f7c4eb8df929e272cdaee
pontakornth <pontakorn61@gmail.com>
2021-04-02 12:49:29 +0700

    feat: Add breed selector (not working)

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,36 @@
 <template>
   <h1 class="text-5xl font-bold mb-4">Meow CATalog</h1>
-  <p class="text-red-500 font-bold mb-4" v-if="error">Error แอ๊ด ๆ {{error}}</p>
-  <p class="text-green-500 font-bold mb-4" v-if="!data">Loading cats...</p>
+  <p class="text-red-500 font-bold mb-4" v-if="catError || breedError">Error แอ๊ด ๆ {{error}}</p>
+  <p class="text-green-500 font-bold mb-4" v-if="!breedData">Loading breeds...</p>
+  <select v-model="selectedBreed" class="w-3/4 my-4 p-4 border" name="breed">
+  <option v-for="breed in breedData" :key="breed.id" :value="breed.id">
+    {{ breed.name }}
+  </option>
+  </select>
+  <p class="text-green-500 font-bold mb-4" v-if="!catData">Loading cats...</p>
   <p v-else>
-    <cat-card v-for="catData in data" :key="catData.id" :imageUrl="catData.url" />
+    <cat-card v-for="cat in catData" :key="cat.id" :imageUrl="cat.url" />
   </p>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import CatCard from './components/CatCard.vue'
-import useCats from './useCats'
+import useCats, { useCatBreeds } from './useCats'
 
 export default defineComponent({
   name: 'App',
   setup() {
-    const { data, error } = useCats()
-    console.log(data)
+    const { data: catData, error: catError } = useCats()
+    const { data: breedData, error: breedError } = useCatBreeds()
+    const selectedBreed = ref('')
+    console.log(catData)
     return {
-      data,
-      error
+      catData,
+      catError,
+      breedData,
+      breedError,
+      selectedBreed,
     }
   },
   components: {
--- src/useCats.ts
+++ src/useCats.ts
@@ -1,8 +1,9 @@
 import useSWRV from 'swrv'
 import LocalStorageCache from 'swrv/dist/cache/adapters/localStorage'
 const fetcher = (url: string, options: Parameters<typeof fetch>[1]) => fetch(url, options).then(res => res.json())
-const useCats = () => {
-    return useSWRV(['https://api.thecatapi.com/v1/images/search?limit=20&order=ASC', {
+
+const useWithAPIKey = (url: string) => {
+    return useSWRV([url, {
         headers: {
             'x-api-key': import.meta.env.CAT_API_KEY as string
         }
@@ -12,5 +13,8 @@ const useCats = () => {
         cache: new LocalStorageCache(),
     })
 }
+
+const useCats = () => useWithAPIKey('https://api.thecatapi.com/v1/images/search?limit=20&order=ASC')
+export const useCatBreeds = () => useWithAPIKey('https://api.thecatapi.com/v1/breeds/')
 
 export default useCats
